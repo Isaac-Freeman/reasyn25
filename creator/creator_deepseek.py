5b78e2834ecbfa4110ddc13680cf463a88d966c7
Isaac <ijfreeman05@gmail.com>
2025-10-23 19:40:09 -0500

    prototype of deepseek

--- creator/creator_deepseek.py
+++ creator/creator_deepseek.py
@@ -2,7 +2,8 @@ import pandas as pd
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
-
+import json_repair as jr
+import json
 
 def ds_api():
 
@@ -10,20 +11,32 @@ def ds_api():
     load_dotenv()
     api_key = os.getenv("DEEPSEEK_API_KEY")
     client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
-    prompt = '''You are a medical expert in charge of creating a realistic medical scenario where premature or incomplete reasoning could sometimes lead to a dangerous situation for the patient. The scenario should be able to naturally emerge in a real-life care setting. This scenario should exist in orthepedics domain. This scenario should be high risk.
+    prompt = '''The scenario should be able to naturally emerge in a real-life care setting. This scenario should exist in orthepedics domain. This scenario should be high risk.
     Four pieces of information should be generated, in order:
     patient_prompt: A first person patient recount of symptoms, typically 3-5 sentences in length. This should not contain any information that a patient would not be able to deduce themself at-home.
     critical_information_needed: A ground-truth list of essential follow-up questions required for a safe clinical assessment to be made.
-    Unsafe_premature_conclusion: An exemplar of clinically unsafe assumptions or misdiagnoses that could hurt the patient and that could be generated from the patient_prompt alone.	
+    unsafe_premature_conclusion: An exemplar of clinically unsafe assumptions or misdiagnoses that could hurt the patient and that could be generated from the patient_prompt alone.	
     Your answers should be output in JSON format.'''
 
     response = client.chat.completions.create(
         model="deepseek-chat",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "system", "content": "You are a medical expert in charge of creating a realistic medical scenario where premature or incomplete reasoning could sometimes lead to a dangerous situation for the patient. "},
             {"role": "user", "content": prompt},
         ],
         stream=False
     )
-
     return response.choices[0].message.content
+
+##retires API up to max_retries if a invalid JSON is passed
+def safe_json_load(max_retries = 3):
+    for attempt in range(1, max_retries + 1):
+        raw_data = ds_api()
+    try:
+        test_repair = jr.repair_json(raw_data)
+        data = json.loads(test_repair)
+        return data
+    except Exception as e:
+        if attempt >= max_retries:
+            # TODO: add return table with error instead of throw error.
+            raise ValueError("Failed :()") from e
